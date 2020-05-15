fe0af0d05713e56afd9e71a841c7ab4d96c39f14
Shrodo <daniel.kvar@gmail.com>
2020-05-15 19:13:57 +0200

    Shranjevanje in prikaz zajete slike.

--- NPO/app/src/main/java/com/example/projekt/MainActivity.java
+++ NPO/app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,25 +1,39 @@
 package com.example.projekt;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    final static int REQUEST_CODE = 0;
+    final static int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageV;
+
+    String currentPhotoPath;
 
     private SensorManager sensorManager;
     Sensor accelerometer;
@@ -35,11 +49,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Button CameraButton = findViewById(R.id.cameraButton);
 
+        imageV = findViewById(R.id.capturedImage);
+
         CameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i, REQUEST_CODE);
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), "com.example.projekt.fileprovider", photoFile);
+                    i.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
 
@@ -55,14 +82,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-
-        super.onActivityResult(requestCode, resultCode, intent);
-        if(resultCode == RESULT_OK){
-            Bundle extras = intent.getExtras();
-            Bitmap data = (Bitmap) extras.get("data");
-            imageV = (ImageView) findViewById(R.id.capturedImage);
-            imageV.setImageBitmap(data);
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_CANCELED){
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                try {
+                    setPic();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -82,5 +113,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             String reading = "X: " + sensorEvent.values[0] + "\n" + "Y: " + sensorEvent.values[1] + "\n" + "Z: " + sensorEvent.values[2];
             accelerometerTV.setText(reading);
         }
+    }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void setPic() {
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        int scaleFactor = 3;
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageV.setImageBitmap(bitmap);
     }
 }
