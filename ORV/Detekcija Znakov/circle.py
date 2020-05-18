52c29d16f4b4dc73fadcc67969df7f2142339e11
AljazLojen <aljazkec@hotmail.com>
2020-05-18 20:04:16 +0200

    Detekcija posodobitev
    Zajem slike iz kamere, ter zaznavanje znaka

--- ORV/Detekcija Znakov/circle.py
+++ ORV/Detekcija Znakov/circle.py
@@ -2,36 +2,58 @@ import numpy as np
 import cv2
 import matplotlib.pylab as plt
 
-#Load the image
-image = cv2.imread("circle.jpg")
+def trafficSignCircle():
+        
+    cap = cv2.VideoCapture(1)
 
 
+    while(cap.isOpened()):
+        _,image = cap.read()
+        blur = cv2.GaussianBlur(image,(5,5),0)
+        hsv_image = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
 
-#Resize image
-"""
-scale_percent = 90 # percent of original size
-width = int(image.shape[1] * scale_percent / 100)
-height = int(image.shape[0] * scale_percent / 100)
-dim = (width, height)
-image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
-"""
+        low = np.array([90,80,50])
+        high = np.array([110,255,255])
+        mask = cv2.inRange(hsv_image,low,high)
+        kernel = np.ones((2,3),np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        color = cv2.bitwise_and(image, image, mask=mask)
 
-#Convert image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Make a treshhold
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        
+        #Make a treshhold
+        ret,thresh = cv2.threshold(hsv_image,127,255,cv2.THRESH_BINARY_INV)    
+        
+        # Find circles with HoughCircles
+        circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, minDist=500 ,param1=100, param2=15, minRadius=25,maxRadius=200)
+        
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            for (x,y,r) in circles:
+                cv2.circle(image, (x,y), r, (0,255,0), 2)
+        
+        cv2.imshow("image",image)
+        cv2.imshow("mask",color)
 
-# Find circles with HoughCircles
-circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, minDist=50 ,param1=100, param2=15, minRadius=10,maxRadius=100)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-if circles is not None:
-    circles = np.round(circles[0, :]).astype("int")
-    for (x,y,r) in circles:
-        cv2.circle(image, (x,y), r, (36,255,12), 2)
+ """
+        #Resize the image
+        scale_percent = 110 # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
+    """
 
-cv2.imshow('thresh', thresh)
-cv2.imshow('image', image)
-cv2.waitKey()
-cv2.destroyAllWindows()
 
+def main():
+    trafficSignCircle()
+
+
+if __name__ == '__main__':
+    main()
--- ORV/Detekcija Znakov/square.py
+++ ORV/Detekcija Znakov/square.py
@@ -1,33 +1,75 @@
 import numpy as np
 import cv2 
+import argparse
 
 
 
-#Load the image
-image = cv2.imread("scrabble.jpg")
+def trafficSignSquare():
+    cap = cv2.VideoCapture(1)
 
-#Resize the image
-scale_percent = 50 # percent of original size
-width = int(image.shape[1] * scale_percent / 100)
-height = int(image.shape[0] * scale_percent / 100)
-dim = (width, height)
-image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
+    while(cap.isOpened()):
+        _, image = cap.read()    
+        blur = cv2.GaussianBlur(image,(5,5),0)
+        hsv_image = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+        
+        low = np.array([90,80,50])
+        high = np.array([110,255,255])
+        mask = cv2.inRange(hsv_image, low, high)
+        kernel = np.ones((3,3),np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+        color = cv2.bitwise_and(image, image, mask=mask)
+        largestArea = 0
+        imageArea = image.shape[0]*image.shape[1]
+
+        contours = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+        if len(contours) > 0: 
+            for contour in contours:
+                    rect = cv2.minAreaRect(contour)
+                    box = cv2.boxPoints(rect)
+                    box = np.int0(box)
+                    
+                    #print("Calculation 1: {} - {} = {}".format(box[0],box[1],box[0]-box[1]))
+                    sideOne = np.linalg.norm(box[0]-box[1])
+                # print("Calculation 2: {} - {} = {}".format(box[0],box[3],box[0]-box[3]))
+                    sideTwo = np.linalg.norm(box[0]-box[3])
+                    # count area of the rectangle
+                    area = sideOne*sideTwo
+
+                    #Find largest contour
+                    if area > largestArea:
+                        largestArea = area
+                        largestRect = box
+                    #print("Largest Area:{}".format(largestArea))
+
+        if largestArea > imageArea*0.02:
+                    cv2.drawContours(image,[largestRect],0,(0,0,255),2)
+                
+
+        cv2.imshow("image",image)
+        cv2.imshow("mask",color)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+        
+
+    """
+        #Resize the image
+        scale_percent = 110 # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
+    """
 
 
-#Convert image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.medianBlur(gray,5)
-
-ret,thresh = cv2.threshold(gray,127,255,0)
-
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-
-cv2.drawContours(image,contours,-1,(0,255,0),3)
-
-cv2.imshow("square",image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+def main():
+    trafficSignSquare()
 
 
-
-
+if __name__ == '__main__':
+    main()
