ec155f01c7afa93e4faa9ca245620432195cb8d7
AljazLojen <aljazkec@hotmail.com>
2020-05-26 09:45:57 +0200

    Scraper Popravek

--- RAIN/scrape/main.py
+++ RAIN/scrape/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+
 def scrape():
     try:
         result = requests.get("https://www.rtvslo.si/stanje-na-cestah")
@@ -14,26 +15,29 @@ def scrape():
     cesta = []
     lok = []
     vzrok = []
+
     for i in found:
         temp = i.text.split(',')
         if len(temp) == 2:
-            cesta.append(temp[0])
-            lok.append(temp[1])
             vzr = temp[1].split(':')
-            vzrok.append(vzr[1])
+            if len(vzr) == 2:
+                cesta.append(temp[0])
+                lok.append(temp[1])
+                vzrok.append(vzr[1])
 
-    data = []
-    #print(len(cesta))
+    data = {}
+    data['nesrece'] = []
+    # print(len(cesta))
     for i in range(0, len(cesta)):
-        data.append({
+        data['nesrece'].append({
             'cesta': cesta[i],
             'lokacija': lok[i],
             'vzrok': vzrok[i]
         })
-    #with open('data.json', 'w') as outfile: 
+    #with open('data.json', 'w') as outfile:
         #json.dump(data, outfile)
 
-
+    
     #Send a post request with the json file to the website
     try:
         response = requests.post('http://localhost:3000/trafficsituation', json=data)
@@ -41,5 +45,7 @@ def scrape():
     except requests.exceptions.RequestException as e:
         print(e)
         raise SystemExit(e)
+    
+
 
 scrape()
